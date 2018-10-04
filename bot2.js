caa4f4ae63b1125a37d2069e46974a0ac4b8366f
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-10-04 10:40:50 +0100

    Add files via upload

---
+++ bot2.js
@@ -0,0 +1,12 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '--SA';
+client.on('ready', function(){    
+    
+});
+
+
+
+///all Reghit Resverd 2018/2019 DyzerYT
+client.login(process.env.BOT_TOKEN2);
