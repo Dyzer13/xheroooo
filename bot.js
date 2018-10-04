84e81077d3204289d4f3e7036fc3eb71e0a9dc4e
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-10-04 10:39:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,21 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '--SA';
 client.on('ready', function(){    
-    var ms = 40000 ;    
-    var setGame = ['تبي تصميم','كلمني خاص'];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
-}, ms);    
-    
 });
 
 
